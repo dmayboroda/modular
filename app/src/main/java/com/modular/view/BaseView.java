e6c6d02b21753bba11b37adaa3c232777956ff2a
David Mayboroda <david.ftzi@gmail.com>
2016-10-31 11:29:25 +0200

    base view for mvp

---
+++ app/src/main/java/com/modular/view/BaseView.java
@@ -0,0 +1,13 @@
+package com.modular.view;
+
+/**
+ * Created by Mayboroda on 10/31/16.
+ */
+
+public interface BaseView {
+
+    void showProgress();
+    void showContent();
+    void showEmpty();
+    void showError(Throwable throwable);
+}
